4226cc62c712de253dbb385022baff9301bc5da6
Thiago Tolotti <thiagotolotti@gmail.com>
2022-09-06 00:15:21 -0300

    Transpiling code for new language features (babel)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // index.js  
-let moment = require('moment')
+import moment from 'moment';
 
 console.log("Hello from JavaScript!");
 
 console.log(moment().startOf('day').fromNow())
 
 console.log('teste do webpack.config.js')
+
+let name = "Bob", time = "today"
+console.log(`Hello ${name}, how are ya ${time}?`)

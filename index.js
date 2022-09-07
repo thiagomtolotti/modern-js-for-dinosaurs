b4b45b8c3fe2e87287f5907174ea4166a6d54094
Thiago Tolotti <thiagotolotti@gmail.com>
2022-09-07 14:16:41 -0300

    Live server com babel funcionando

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ console.log("Hello from JavaScript!");
 
 console.log(moment().startOf('day').fromNow())
 
-<<<<<<< HEAD
 console.log('teste do webpack.config.js')
 
 let name = "Bob", time = "today"
 console.log(`Hello ${name}, how are ya ${time}?`)
 
-console.log('saddfas')
-=======
-console.log('teste do 10.config.js')
->>>>>>> hot_reload
+console.log('saddfasavab')
